ec0400a53841e6f83a09fbcfd952ae602e02226e
Nikita_Pohodski <byteek.p.n@gmail.com>
2020-10-10 11:59:15 +0300

    test heroku

--- src/main/java/by/academy/controller/mining/MiningController.java
+++ src/main/java/by/academy/controller/mining/MiningController.java
@@ -14,9 +14,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+//import java.net.http.HttpClient;
+//import java.net.http.HttpRequest;
+//import java.net.http.HttpResponse;
 
 @Controller
 @RequestMapping("/mining")
@@ -38,21 +38,21 @@ public class MiningController {
          AppUser appUser = userService.getUserByUsername(UserService.getUsernameAuthUser());
         String appUserJson = new GsonBuilder().setPrettyPrinting().create().toJson(appUser);
 
-        postRequestToTheMiningService(appUserJson);
+//        postRequestToTheMiningService(appUserJson);
         return "redirect:home";
     }
 
 
-    private static HttpResponse<String>
-    postRequestToTheMiningService(String body) throws IOException, InterruptedException {
-        HttpClient httpClient = HttpClient.newHttpClient();
-
-        HttpRequest httpRequest = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8081/runMining"))
-                .POST(HttpRequest.BodyPublishers.ofString(body))
-                .header("Content-Type", "application/json")
-                .build();
-
-        return httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-    }
+//    private static HttpResponse<String>
+//    postRequestToTheMiningService(String body) throws IOException, InterruptedException {
+//        HttpClient httpClient = HttpClient.newHttpClient();
+//
+//        HttpRequest httpRequest = HttpRequest.newBuilder()
+//                .uri(URI.create("http://localhost:8081/runMining"))
+//                .POST(HttpRequest.BodyPublishers.ofString(body))
+//                .header("Content-Type", "application/json")
+//                .build();
+//
+//        return httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+//    }
 }
