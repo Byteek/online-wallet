1a72fde27f77e978ca8b0e07ae199da3b79b1dc2
Nikita_Pohodski <byteek.p.n@gmail.com>
2020-10-11 12:23:48 +0300

    test heroku

--- src/main/java/by/academy/controller/HomeController.java
+++ src/main/java/by/academy/controller/HomeController.java
@@ -3,8 +3,7 @@ package by.academy.controller;
 import by.academy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,7 +13,7 @@ public class HomeController {
     @Autowired
     UserService userService;
 
-    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    @GetMapping(value = "/home")
     public ModelAndView homePage(ModelAndView modelAndView) {
         modelAndView.setViewName("home"); // -> /WEB-INF/jsp/ + home + .jsp
 
